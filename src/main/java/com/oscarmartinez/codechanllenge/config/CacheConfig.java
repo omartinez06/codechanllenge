eea75e3dde493dbf1c4cea1550080f2292ba29d2
omartinez06 <omartinezordonez@gmail.com>
2025-05-20 14:48:21 -0600

    update

--- src/main/java/com/oscarmartinez/codechanllenge/config/CacheConfig.java
+++ src/main/java/com/oscarmartinez/codechanllenge/config/CacheConfig.java
@@ -14,7 +14,7 @@ public class CacheConfig {
     @Bean
     public Caffeine<Object, Object> caffeineConfig() {
         return Caffeine.newBuilder()
-                .expireAfterWrite(30, TimeUnit.MINUTES) // Guarda por 30 minutos
+                .expireAfterWrite(30, TimeUnit.MINUTES)
                 .maximumSize(100);
     }
 
